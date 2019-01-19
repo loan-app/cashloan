50cf5084e5fa7b27432765a791353732af533989
szb <zhenbo.shi@youngstack.com>
2019-01-19 15:55:08 +0800

    mapper文件修改

--- cashloan-cl/src/main/java/com/xiji/cashloan/cl/mapper/DecisionMapper.java
+++ cashloan-cl/src/main/java/com/xiji/cashloan/cl/mapper/DecisionMapper.java
@@ -1,13 +1,19 @@
 package com.xiji.cashloan.cl.mapper;
 
-import com.xiji.cashloan.cl.domain.Decision;
 import com.xiji.cashloan.core.common.mapper.BaseMapper;
 import com.xiji.cashloan.core.common.mapper.RDBatisDao;
+import com.xiji.cashloan.cl.domain.Decision;
 
 /**
  * 决策数据Dao
- * Created by szb on 19/1/19.
+ * 
+ * @author szb
+ * @version 1.0.0
+ * @date 2019-01-19 15:53:20
  */
 @RDBatisDao
-public interface DecisionMapper extends BaseMapper<Decision,Long> {
+public interface DecisionMapper extends BaseMapper<Decision, Long> {
+
+    
+
 }
