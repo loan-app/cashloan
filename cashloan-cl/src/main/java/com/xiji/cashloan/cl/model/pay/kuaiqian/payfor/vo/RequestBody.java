f0462a41eecb0332a392f9a4c90c407b484fe8c1
wang <1910975064@qq.com>
2019-04-27 16:25:35 +0800

    块钱统一类结构

---
+++ cashloan-cl/src/main/java/com/xiji/cashloan/cl/model/pay/kuaiqian/payfor/vo/RequestBody.java
@@ -0,0 +1,29 @@
+package com.xiji.cashloan.cl.model.pay.kuaiqian.payfor.vo;
+
+import javax.xml.bind.annotation.*;
+
+/**
+ * 报文实体
+ * @author zan.liang
+ *
+ */
+@XmlAccessorType(XmlAccessType.FIELD)  
+@XmlRootElement  
+@XmlType(name = "requestBody", propOrder = {"sealDataType"})  
+public class RequestBody {
+
+	
+	@XmlElement(name = "sealDataType")  
+	private SealDataType sealDataType;
+
+	public SealDataType getSealDataType() {
+		return sealDataType;
+	}
+
+	public void setSealDataType(SealDataType sealDataType) {
+		this.sealDataType = sealDataType;
+	}
+
+
+	
+}
