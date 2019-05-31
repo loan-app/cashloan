d55b6a910b36b7a0093286db43d8783437d5c9a0
xyf <2470737873@qq.com>
2019-05-31 12:02:31 +0800

    未借用户信息改为未复借用户信息

--- doc/database/dBChangeLog.sql
+++ doc/database/dBChangeLog.sql
@@ -961,11 +961,11 @@ INSERT INTO `arc_sys_menu` VALUES ('1028', '0', '实时到期还款统计', '101
 INSERT INTO `arc_sys_role_menu` VALUES (null, '1', '1028');
 
 -- 添加用户管理列表 未借款用户信息
-INSERT INTO `arc_sys_menu` VALUES ('1025', '0', '未借用户信息', '2', '', null, '00000000006', null, '', null, '', '未借用户信息', '0', 'UserNotBorrowAgain', null, null, null, null);
+INSERT INTO `arc_sys_menu` VALUES ('1025', '0', '未复借用户信息', '2', '', null, '00000000006', null, '', null, '', '未借用户信息', '0', 'UserNotBorrowAgain', null, null, null, null);
 
 INSERT INTO `arc_sys_role_menu` VALUES (null, '1', '1025');
 
 -- 根据完成次数提额
 INSERT INTO `arc_sys_config` VALUES (null, '20', '还款提额次数', 'count_improve_credit', '1,1,1', '1', '参照还款提额次数(1,2,3)格式每个数字代表当次提额要还款的次数,数字的个数参照提额上限除以单次增加的额度', '1');
 --  添加成功还款次数字段
-ALTER TABLE arc_credit add column `num` int(11) DEFAULT '0' COMMENT '有效还款次数' after count;
+ALTER TABLE arc_credit add column `num` int(11) DEFAULT '0' COMMENT '当次有效还款次数' after count;
